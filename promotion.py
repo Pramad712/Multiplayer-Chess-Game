72ba278c060e2c2ca9d1872f7025c6bb2ac94273
Pramad712 <77818951+Pramad712@users.noreply.github.com>
2021-08-10 16:35:12 -0700

    Update promotion.py

--- promotion.py
+++ promotion.py
@@ -4,87 +4,125 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from constants import *
 import sys
-import os
 
-# Clicked Functions
-def queen_clicked(menu, piece):
-    if piece.color == "white":
-        piece.image = white_queen
+width, height = 100, 100
 
-    elif piece.color == "black":
-        piece.image = black_queen
+class promotion_menu():
+    # Cancel Text
+    def __init__(self, piece):
+        self.piece = piece
 
-    piece.promotion_letter = "q"
-    menu.close()
+    # Clicked Functions
+    def exit_clicked(self, menu):
+        self.close(menu)
 
-def rook_clicked(menu, piece):
-    if piece.color == "white":
-        piece.image = white_rook
+        if self.piece.type == "pawn":
+            self.promote = False
 
-    elif piece.color == "black":
-        piece.image = black_rook
+    def close(self, menu):
+        menu.close()
 
-    piece.promotion_letter = "r"
-    menu.close()
+    def queen_clicked(self, menu):
+        if self.piece.color == "white":
+            self.piece.image = white_queen
 
-def bishop_clicked(menu, piece):
-    if piece.color == "white":
-        piece.image = white_bishop
+        elif self.piece.color == "black":
+            self.piece.image = black_queen
 
-    elif piece.color == "black":
-        piece.image = black_bishop
+        self.piece.promotion_letter = "q"
+        self.piece.type = "queen"
+        self.promote = True
+        self.close(menu)
 
-    piece.promotion_letter = "b"
-    menu.close()
+    def rook_clicked(self, menu):
+        if self.piece.color == "white":
+            self.piece.image = white_rook
 
-def knight_clicked(menu, piece):
-    if piece.color == "white":
-        piece.image = white_knight
+        elif self.piece.color == "black":
+            self.piece.image = black_rook
 
-    elif piece.color == "black":
-        piece.image = black_knight
+        self.piece.promotion_letter = "r"
+        self.piece.type = "rook"
+        self.promote = True
+        self.close(menu)
 
-    piece.promotion_letter = "n"
-    menu.close()
+    def bishop_clicked(self, menu):
+        if self.piece.color == "white":
+            self.piece.image = white_bishop
 
-def promotion_menu(piece):
-    try:
-        # Using PyQt5 as Pygame 2.0 doesn't support multiple windows yet.
-        # A list of the pieces the user can promote their pawn to
-        wn = QApplication(sys.argv)
+        elif self.piece.color == "black":
+            self.piece.image = black_bishop
 
-        # Centering the menu in the middle of my screen (my screen is 1920 x 1080 pixels)
-        menu = QMainWindow()
-        menu.setGeometry(960 - 2 * square_width, 540 - square_height / 2, 4 * square_width, square_height)
+        self.piece.promotion_letter = "b"
+        self.piece.type = "bishop"
+        self.promote = True
+        self.close(menu)
 
-        # Dawing the Buttons
-        queen_button = QPushButton(menu)
-        queen_button.setIcon(QIcon(piece.promotion_pieces_directories[0]))
-        queen_button.setIconSize(QSize(square_width, square_height))
-        queen_button.setGeometry(0, 0, square_width, square_height)
-        rook_button = QPushButton(menu)
-        rook_button.setIcon(QIcon(piece.promotion_pieces_directories[1]))
-        rook_button.setIconSize(QSize(square_width, square_height))
-        rook_button.setGeometry(85, 0, square_width, square_height)
-        bishop_button = QPushButton(menu)
-        bishop_button.setIcon(QIcon(piece.promotion_pieces_directories[2]))
-        bishop_button.setIconSize(QSize(square_width, square_height))
-        bishop_button.setGeometry(170, 0, square_width, square_height)
-        knight_button = QPushButton(menu)
-        knight_button.setIcon(QIcon(piece.promotion_pieces_directories[3]))
-        knight_button.setIconSize(QSize(square_width, square_height))
-        knight_button.setGeometry(255, 0, square_width, square_height)
+    def knight_clicked(self, menu):
+        if self.piece.color == "white":
+            self.piece.image = white_knight
 
-        # Effect when a button is clicked
-        queen_button.clicked.connect(lambda: queen_clicked(menu, piece))
-        rook_button.clicked.connect(lambda: rook_clicked(menu, piece))
-        bishop_button.clicked.connect(lambda: bishop_clicked(menu, piece))
-        knight_button.clicked.connect(lambda: knight_clicked(menu, piece))
+        elif self.piece.color == "black":
+            self.piece.image = black_knight
 
-        # Display Screen
-        menu.show()
-        wn.exec_()
+        self.piece.promotion_letter = "n"
+        self.piece.type = "knight"
+        self.promote = True
+        self.close(menu)
+
+    def draw_menu(self):
+        try:
+            # A list of the self.pieces the user can promote their pawn to
+            wn = QApplication(sys.argv)
+
+            menu = QMainWindow()
+            menu.setWindowTitle("Your Pawn Promotes to a ...")
+            menu.setFixedSize(4 * width, height) # Makes it not resizable
+
+            # Drawing the Buttons
+            # Promotion Buttons
+            # Queen Button
+            queen_button = QPushButton(menu)
+            queen_button.setStyleSheet("background: rgb(127.5, 127.5, 127.5)")
+            queen_button.setIcon(QIcon(self.piece.promotion_pieces_directories[0]))
+            queen_button.setIconSize(QSize(width, height))
+            queen_button.setGeometry(0, 0, width, height)
 
 
-    except:
-        pass
+            # Knight Button
+            knight_button = QPushButton(menu)
+            knight_button.setStyleSheet("background: rgb(127.5, 255, 127.5)")
+            knight_button.setIcon(QIcon(self.piece.promotion_pieces_directories[3]))
+            knight_button.setIconSize(QSize(width, height))
+            knight_button.setGeometry(width, 0, width, height)
+
+            # Rook Button
+            rook_button = QPushButton(menu)
+            rook_button.setStyleSheet("background: rgb(0, 255, 127.5)")
+            rook_button.setIcon(QIcon(self.piece.promotion_pieces_directories[1]))
+            rook_button.setIconSize(QSize(width, height))
+            rook_button.setGeometry(2 * width, 0, width, height)
+
+            # Bishop Button
+            bishop_button = QPushButton(menu)
+            bishop_button.setStyleSheet("background: rgb(0, 176, 169)")
+            bishop_button.setIcon(QIcon(self.piece.promotion_pieces_directories[2]))
+            bishop_button.setIconSize(QSize(width, height))
+            bishop_button.setGeometry(3 * width, 0, width, height)
+
+            # Effect when a button is clicked
+            queen_button.clicked.connect(lambda: self.queen_clicked(menu))
+            rook_button.clicked.connect(lambda: self.rook_clicked(menu))
+            bishop_button.clicked.connect(lambda: self.bishop_clicked(menu))
+            knight_button.clicked.connect(lambda: self.knight_clicked(menu))
+
+            # Close Button
+            wn.aboutToQuit.connect(lambda: self.exit_clicked(menu))
+
+            # Display Screen
+            menu.show()
+            wn.exec_()
+
+
+        except:
+            pass
