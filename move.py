b07a970dd871f9eaf147f3419c2affb987856733
Pramad712 <77818951+Pramad712@users.noreply.github.com>
2021-08-10 16:33:33 -0700

    Update move.py

--- move.py
+++ move.py
@@ -2,8 +2,10 @@ import chess
 from constants import *
 from special_moves import *
 
-def move_piece(board_interpretation, original_square, resulting_square, pieces):
+def move_piece(board_interpretation, original_square, resulting_square, pieces, captured_pieces):
     # updating board interpretation
+
+    capture = False
     # Indexs, selection
     file_n = files.index(original_square[0])
     rank_n = 7 - ranks.index(original_square[1])
@@ -14,19 +16,19 @@ def move_piece(board_interpretation, original_square, resulting_square, pieces):
     place_moved = board_interpretation[moved_rank_n][moved_file_n]
 
     if place_moved != None:  # Capture
+        capture = True
         location = pieces.index(place_moved)
         del pieces[location]
+        captured_pieces.append(place_moved)
 
     board_interpretation[rank_n][file_n] = None
     board_interpretation[moved_rank_n][moved_file_n] = piece
 
+    return capture
 
 def update_board(board, original_square, resulting_square, piece_promoted):
-    try:
-        # update board from chess module
-        move = original_square + resulting_square + piece_promoted
-        move = chess.Move.from_uci(move)
-        board.push(move)
+    # update board from/using chess module
+    move = original_square + resulting_square + piece_promoted
+    move = chess.Move.from_uci(move)
+    board.push(move)
 
-    except:
-        pass
