2d8566542f9dc11b56567bd4d5d289b9ff4bd832
Pramad712 <77818951+Pramad712@users.noreply.github.com>
2021-11-24 19:01:19 -0800

    Update legal_moves.py

--- legal_moves.py
+++ legal_moves.py
@@ -1,7 +1,8 @@
 import pygame
+import chess
 from constants import *
 
-def draw_legal_moves(WN, square, color, board, turn):
+def draw_legal_moves(WN: pygame.Surface, square: str, color: str, board: chess.Board, turn: int) -> list:
     # Using chess module to find legal moves based on chess.Board.legal_moves
     squares = []
 
@@ -10,21 +11,16 @@ def draw_legal_moves(WN, square, color, board, turn):
             move = str(move)  # Can't not do this because move isn't type string, but type chess.Move
             if square in move:
                 legal_square = move.replace(square, "")
-                if len(legal_square) == 3:
-                    legal_square = legal_square.replace(legal_square[2], "")
+                if len(legal_square) > 2:
+                    legal_square = legal_square.replace(legal_square[2:], "")
 
                 squares.append(legal_square)
 
 
     for square in squares:
-        try:
-            x = DIF_BOARD + (files.index(square[0]) + 0.5) * square_width
-            y = (8 - ranks.index(square[1]) - 0.5) * square_height + timer_height
-            pygame.draw.circle(WN, CYAN, (x, y), ((square_width + square_height)/6))
-            pygame.display.update()
-
-        except:
-            pass
+        x = DIF_BOARD + (files.index(square[0]) + 0.5) * square_width
+        y = (8 - ranks.index(square[1]) - 0.5) * square_height + timer_height
+        pygame.draw.circle(WN, CYAN, (x, y), ((square_width + square_height)/6))
+        pygame.display.update()
 
     return squares
-
