c244416b939d419c0eeb556d299bb5fe32e878a5
Pramad712 <77818951+Pramad712@users.noreply.github.com>
2021-11-24 19:03:24 -0800

    Update move.py

--- move.py
+++ move.py
@@ -1,18 +1,17 @@
 import chess
+from numpy import ndarray
 from constants import *
 from special_moves import *
 
-def move_piece(board_interpretation, original_square, resulting_square, pieces, captured_pieces):
-    # updating board interpretation
-
+def move_piece_normally(board_interpretation: numpy.ndarray, original_square: str, resulting_square: str, pieces: list, captured_pieces: list) -> bool:
     capture = False
-    # Indexs, selection
-    file_n = files.index(original_square[0])
-    rank_n = 7 - ranks.index(original_square[1])
 
+    past_file_n = files.index(original_square[0])
+    past_rank_n = 7 - ranks.index(original_square[1])
     moved_file_n = files.index(resulting_square[0])
     moved_rank_n = 7 - ranks.index(resulting_square[1])
-    piece = board_interpretation[rank_n][file_n]
+
+    piece = board_interpretation[past_rank_n][past_file_n]
     place_moved = board_interpretation[moved_rank_n][moved_file_n]
 
     if place_moved != None:  # Capture
@@ -21,13 +20,12 @@ def move_piece(board_interpretation, original_square, resulting_square, pieces, 
         del pieces[location]
         captured_pieces.append(place_moved)
 
-    board_interpretation[rank_n][file_n] = None
+    board_interpretation[past_rank_n][past_file_n] = None
     board_interpretation[moved_rank_n][moved_file_n] = piece
 
     return capture
 
-def update_board(board, original_square, resulting_square, piece_promoted):
-    # update board from/using chess module
+def update_board(board: chess.Board, original_square: str, resulting_square: str, piece_promoted: str) -> None:
     move = original_square + resulting_square + piece_promoted
     move = chess.Move.from_uci(move)
     board.push(move)
