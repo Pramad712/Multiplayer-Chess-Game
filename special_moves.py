f8be586b077ff51807a50897a75b97ca256dd837
Pramad712 <77818951+Pramad712@users.noreply.github.com>
2021-11-24 19:04:04 -0800

    Update special_moves.py

--- special_moves.py
+++ special_moves.py
@@ -1,15 +1,16 @@
 # Castling, En Passant, Promotions
 import numpy
 from constants import *
-from promotion import promotion_menu
+from board import Piece
 
-def detect_special_moves(board_interpretation, piece, original_square, resulting_square):
+def detect_special_moves(board_interpretation: numpy.ndarray, piece: Piece, original_square: str, resulting_square: str) -> tuple[bool, bool, bool]:
     moved_file_n = files.index(resulting_square[0])
     moved_rank_n = ranks.index(resulting_square[1])
 
     # Detecting Special Moves
     # Castling
     castling = False
+
     # There is an exception which is if the rook and king are of different colors, but the king would be in check.
     if piece.type == "king" and (files.index(resulting_square[0]) - files.index(original_square[0]) == 2 or files.index(resulting_square[0]) - files.index(original_square[0]) == -2) and original_square[1] == resulting_square[1]:
         castling = True
@@ -26,23 +27,35 @@ def detect_special_moves(board_interpretation, piece, original_square, resulting
         left = location[1][0] - 1
         right = location[1][0] + 1
 
-        try:
-            # White Pieces (move up)
-            if board_interpretation[up][left] == None and left == moved_file_n and up == 7 - moved_rank_n:
-                en_passant = True
+        if piece.color == "white":
+            try:
+                if board_interpretation[up][left] == None and left == moved_file_n and up == 7 - moved_rank_n:
+                    en_passant = True
 
-            elif board_interpretation[up][right] == None and right == moved_file_n and up == 7 - moved_rank_n:
-                en_passant = True
+            except:
+                pass
 
-            # Black Pieces (move down)
-            elif board_interpretation[down][left] == None and left == moved_file_n and down == 7 - moved_rank_n:
-                en_passant = True
+            try:
+                if board_interpretation[up][right] == None and right == moved_file_n and up == 7 - moved_rank_n:
+                    en_passant = True
 
-            elif board_interpretation[down][right] == None and right == moved_file_n and down == 7 - moved_rank_n:
-                en_passant = True
+            except:
+                pass
 
-        except:
-            pass
+        if piece.color == "black":
+            try:
+                if board_interpretation[down][left] == None and left == moved_file_n and down == 7 - moved_rank_n:
+                    en_passant = True
+
+            except:
+                pass
+
+            try:
+                if board_interpretation[down][right] == None and right == moved_file_n and down == 7 - moved_rank_n:
+                    en_passant = True
+
+            except:
+                pass
 
     # Promotions
     promotion = False
@@ -52,70 +65,33 @@ def detect_special_moves(board_interpretation, piece, original_square, resulting
 
     return castling, en_passant, promotion
 
-def move_castling(board_interpretation, original_square, resulting_square):
-    def get_location(board_interpretation, king, rook):
-        # Locations
-        king_location = numpy.where(board_interpretation == king)
-        rook_location = numpy.where(board_interpretation == rook)
-        return king_location, rook_location
-
-    file_n = files.index(original_square[0])
-    rank_n = ranks.index(original_square[1])
-    moved_file_n = files.index(resulting_square[0])
-    moved_rank_n = ranks.index(resulting_square[1])
-
-    rook = None
-    king_location, rook_location = [], []
-
-    # Searching for king being used for castling
-    king = board_interpretation[7 - rank_n][file_n]
-
-    # Changing Position of Pieces
-    # Place Pieces
-
-    # Finding which side to castle
-    if files.index(original_square[0]) < files.index(resulting_square[0]): # Kingside Castling
-        # Find Rook
-        rook = board_interpretation[7 - moved_rank_n][moved_file_n + 1]
-
-        # Locations
-        king_location, rook_location = get_location(board_interpretation, king, rook)
-
-        # Placing Pieces
-        board_interpretation[king_location[0][0]][king_location[1][0] + 2] = king
-        board_interpretation[rook_location[0][0]][rook_location[1][0] - 2] = rook
-
-    elif files.index(original_square[0]) > files.index(resulting_square[0]): # Queenside Castling
-        # Find Rook
-        rook = board_interpretation[7 - moved_rank_n][moved_file_n - 2]
-
-        # Locations
-        king_location, rook_location = get_location(board_interpretation, king, rook)
-
-        board_interpretation[king_location[0][0]][king_location[1][0] - 2] = king
-        board_interpretation[rook_location[0][0]][rook_location[1][0] + 3] = rook
-
-    # Removing Pieces
-    board_interpretation[king_location] = None
-    board_interpretation[rook_location] = None
-
-    return board_interpretation
-
-def move_en_passant(board_interpretation, pieces, captured_pieces, original_square, resulting_square):
-    # Find the captured piece
-    file_n = files.index(original_square[0])
-    rank_n = 7 - ranks.index(original_square[1])
+def move_castling(board_interpretation: numpy.ndarray, original_square: str, resulting_square: str) -> None:
+    past_file_n = files.index(original_square[0])
+    past_rank_n = 7 - ranks.index(original_square[1])
     moved_file_n = files.index(resulting_square[0])
     moved_rank_n = 7 - ranks.index(resulting_square[1])
-    piece_captured = None
 
-    # En Passants for white
-    if rank_n > moved_rank_n:
-        piece_captured = board_interpretation[moved_rank_n + 1][moved_file_n]
+    # Move the King
+    board_interpretation[moved_rank_n][moved_file_n] = board_interpretation[past_rank_n][past_file_n]
+    board_interpretation[past_rank_n][past_file_n] = None
 
-    # En Passants for black:
-    elif rank_n < moved_rank_n:
-        piece_captured = board_interpretation[moved_rank_n - 1][moved_file_n]
+    # Move the Rook
+    if moved_file_n > past_file_n:  # Kingside Castling
+        board_interpretation[moved_rank_n][5] = board_interpretation[moved_rank_n][7]
+        board_interpretation[moved_rank_n][7] = None
+
+    if moved_file_n < past_file_n:  # Queenside Castling
+        board_interpretation[moved_rank_n][3] = board_interpretation[moved_rank_n][0]
+        board_interpretation[moved_rank_n][0] = None
+
+def move_en_passant(board_interpretation: numpy.ndarray, pieces: list, captured_pieces: list, original_square: str, resulting_square: str) -> None:
+    # Find the captured piece
+    past_file_n = files.index(original_square[0])
+    past_rank_n = 7 - ranks.index(original_square[1])
+    moved_file_n = files.index(resulting_square[0])
+    moved_rank_n = 7 - ranks.index(resulting_square[1])
+
+    piece_captured = board_interpretation[past_rank_n][moved_file_n]
 
     # Delete Piece from pieces list, and add it to captured_pieces
     location = pieces.index(piece_captured)
@@ -127,6 +103,6 @@ def move_en_passant(board_interpretation, pieces, captured_pieces, original_squa
     board_interpretation[location] = None
 
     # Move Piece
-    piece = board_interpretation[rank_n][file_n]
-    board_interpretation[rank_n][file_n] = None
+    piece = board_interpretation[past_rank_n][past_file_n]
+    board_interpretation[past_rank_n][past_file_n] = None
     board_interpretation[moved_rank_n][moved_file_n] = piece
