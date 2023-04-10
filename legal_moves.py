fe9c9b0f721e4c6523ddbd021257c4e2d3877255
Pramad712 <77818951+Pramad712@users.noreply.github.com>
2023-04-09 18:38:29 -0700

    Update legal_moves.py

--- legal_moves.py
+++ legal_moves.py
@@ -5,20 +5,17 @@ from constants import *
 def draw_legal_moves(WN: pygame.Surface, square: str, color: str, board: chess.Board, turn: int) -> list:
     # Using chess module to find legal moves based on chess.Board.legal_moves
     squares = []
-    print(colors, color, turn)
 
     for move in board.legal_moves:
         move = str(move)  # Can't not do this because move isn't type string, but type chess.Move
 
         if square in move:
-            print(move, end=" ")
             legal_square = move[2:]
 
             if len(legal_square) > 2:
                 legal_square = legal_square[:2]
 
             squares.append(legal_square)
-            print(legal_square)
 
     for square in squares:
         print(square, end=" ")
